caf9f8bb725fb1f8e7c2680e3ac077b36fe31b5f
Meera <patelmeeray@gmail.com>
2020-09-25 11:31:54 -0400

    update datatypes

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,13 @@
-library(broca)
-CLASSIFICATION <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CLASSIFICATION.csv')
-CONCEPT <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT.csv')
-CONCEPT_SYNONYM <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT_SYNONYM.csv')
-LINKS_TO_RESOURCES <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/LINKS_TO_RESOURCES.csv')
-NAMES_AND_SYNONYMS <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/NAMES_AND_SYNONYMS.csv')
-REGISTRY_NUMBERS <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBERS.csv')
-REGISTRY_NUMBER_LOG <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBER_LOG.csv')
-RN_URL_VALIDITY <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/RN_URL_VALIDITY.csv')
+library(readr)
+
+CLASSIFICATION <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CLASSIFICATION.csv')
+CONCEPT <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT.csv')
+CONCEPT_SYNONYM <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT_SYNONYM.csv')
+LINKS_TO_RESOURCES <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/LINKS_TO_RESOURCES.csv')
+NAMES_AND_SYNONYMS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/NAMES_AND_SYNONYMS.csv')
+REGISTRY_NUMBERS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBERS.csv')
+REGISTRY_NUMBER_LOG <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBER_LOG.csv')
+RN_URL_VALIDITY <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/RN_URL_VALIDITY.csv')
 
 usethis::use_data(
 	CLASSIFICATION,
