95a94790770a3cf93430c67a1cab8310eeece132
Meera <patelmeeray@gmail.com>
2020-10-08 12:04:29 -0400

    update data package

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' @title CLASSIFICATION
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 1055 rows and 3 variables:
+#' @format A data frame with 8178 rows and 3 variables:
 #' \describe{
 #'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
@@ -9,75 +9,41 @@
 #' @details DETAILS
 "CLASSIFICATION"
 
-#' @title CONCEPT
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 932 rows and 10 variables:
-#' \describe{
-#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{domain_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{vocabulary_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_class_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{standard_concept}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_code}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_start_date}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_end_date}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{invalid_reason}}{character COLUMN_DESCRIPTION} 
-#'}
-#' @details DETAILS
-"CONCEPT"
-
-#' @title CONCEPT_SYNONYM
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 7828 rows and 3 variables:
-#' \describe{
-#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_synonym_name}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{language_concept_id}}{character COLUMN_DESCRIPTION} 
-#'}
-#' @details DETAILS
-"CONCEPT_SYNONYM"
-
 #' @title LINKS_TO_RESOURCES
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 15075 rows and 4 variables:
+#' @format A data frame with 77334 rows and 6 variables:
 #' \describe{
+#'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{ltr_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{resource_agency}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{resource_link}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION} 
+#'   \item{\code{resource_link}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "LINKS_TO_RESOURCES"
 
 #' @title NAMES_AND_SYNONYMS
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 10296 rows and 4 variables:
+#' @format A data frame with 75837 rows and 6 variables:
 #' \describe{
-#'   \item{\code{nas_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{nas_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{concept_synonym_type}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_synonym_name}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "NAMES_AND_SYNONYMS"
 
-#' @title REGISTRY_NUMBERS
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 3346 rows and 4 variables:
-#' \describe{
-#'   \item{\code{rn_datetime}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_registry_number_type}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_registry_number}}{character COLUMN_DESCRIPTION} 
-#'}
-#' @details DETAILS
-"REGISTRY_NUMBERS"
-
 #' @title REGISTRY_NUMBER_LOG
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 6635 rows and 11 variables:
+#' @format A data frame with 24590 rows and 13 variables:
 #' \describe{
+#'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{rnl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{raw_concept}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{processed_concept}}{character COLUMN_DESCRIPTION}
@@ -87,18 +53,33 @@
 #'   \item{\code{no_record}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{response_recorded}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{compound_match}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{rn}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION} 
+#'   \item{\code{rn}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "REGISTRY_NUMBER_LOG"
 
+#' @title REGISTRY_NUMBERS
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 20374 rows and 6 variables:
+#' \describe{
+#'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{rn_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_registry_number_type}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{concept_registry_number}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"REGISTRY_NUMBERS"
+
 #' @title RN_URL_VALIDITY
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 1016 rows and 3 variables:
+#' @format A data frame with 11405 rows and 5 variables:
 #' \describe{
-#'   \item{\code{rnuv_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{c_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_classification}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{rnuv_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{is_404}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,18 +1,13 @@
 library(readr)
-
-CLASSIFICATION <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CLASSIFICATION.csv')
-CONCEPT <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT.csv')
-CONCEPT_SYNONYM <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/CONCEPT_SYNONYM.csv')
-LINKS_TO_RESOURCES <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/LINKS_TO_RESOURCES.csv')
-NAMES_AND_SYNONYMS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/NAMES_AND_SYNONYMS.csv')
-REGISTRY_NUMBERS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBERS.csv')
-REGISTRY_NUMBER_LOG <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/REGISTRY_NUMBER_LOG.csv')
-RN_URL_VALIDITY <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData/data-raw/RN_URL_VALIDITY.csv')
+CLASSIFICATION <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/CLASSIFICATION.csv')
+LINKS_TO_RESOURCES <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/LINKS_TO_RESOURCES.csv')
+NAMES_AND_SYNONYMS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/NAMES_AND_SYNONYMS.csv')
+REGISTRY_NUMBER_LOG <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/REGISTRY_NUMBER_LOG.csv')
+REGISTRY_NUMBERS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/REGISTRY_NUMBERS.csv')
+RN_URL_VALIDITY <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/chemidplusSearchData//data-raw/RN_URL_VALIDITY.csv')
 
 usethis::use_data(
 	CLASSIFICATION,
-	CONCEPT,
-	CONCEPT_SYNONYM,
 	LINKS_TO_RESOURCES,
 	NAMES_AND_SYNONYMS,
 	REGISTRY_NUMBER_LOG,
